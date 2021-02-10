8966896e25723e22d38c6cf8b21302fe838c7ec6
dbryan6 <david.bryan@dxc.com>
2021-02-10 09:18:45 +0000

    Update strava.js

--- strava.js
+++ strava.js
@@ -12,6 +12,32 @@ function getClubActivites(res){
     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}&page=5&per_page=10`
     fetch(activities_link)
         .then((res) => console.log(res.json()))
+
+<h2>Make a table based on JSON data.</h2>
+
+<p id="demo"></p>
+
+<script>
+var obj, dbParam, xmlhttp, myObj, x, txt = "";
+obj = { table: "customers", limit: 20 };
+dbParam = JSON.stringify(obj);
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    myObj = JSON.parse(this.responseText);
+    txt += "<table border='1'>"
+    for (x in myObj) {
+      txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+    }
+    txt += "</table>"    
+    document.getElementById("demo").innerHTML = txt;
+  }
+};
+xmlhttp.open("POST", "json_demo_html_table.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
+</script>
+
 }
 function reAuthorize(){
     fetch(auth_link,{
